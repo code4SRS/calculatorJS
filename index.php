ad118b9ac644f266e0c5ab29f56d50b8907eb0c3
cjseeger <cjseeger@iastate.edu>
2016-09-12 15:30:02 -0500

    initial HTML file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<style>
+html, body {
+    /*height:100%;*/
+    margin:0;
+    padding:0;
+	height: 100vh;
+}
+</style>
+
+<script>
+
+</script>
+</head>
+
+<body>
+<h3>The Calculator Project</h3>
+<hr>
+</body>
+</html>
